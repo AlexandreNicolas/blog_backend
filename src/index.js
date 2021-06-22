d306721e51bbfd628cd1eb4ea451b0c7fe72b71d
Alexandre Seemund <alexandreseemund@gmail.com>
2021-06-21 23:37:03 -0300

    Add .env to deploy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require("express");
+const cors = require('cors')
 const mongoose = require("mongoose");
 const blogRouter = require("./routes/blogRoutes");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
   "mongodb+srv://" + process.env.MONGODB_USER + ":" + process.env.MONGODB_PSWD + 
@@ -18,6 +20,6 @@ mongoose.connect(
 
 app.use(blogRouter);
 
-app.listen(3001, () => {
+app.listen(process.env.PORT || 3001, () => {
   console.log("Server is running...");
 });
